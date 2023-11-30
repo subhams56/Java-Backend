318b4e97dc5057baea8db8fed2013708975dd3ab
Subham Saha <subhams56.ss@gmail.com>
2023-11-30 15:34:37 +0530

    .

--- src/main/java/com/java/backend/api/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.backend.api.repositories;
-
-import com.java.backend.api.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User , Integer> {
-    User findByEmail(String email);
-    User findByUsername(String username);
-}
