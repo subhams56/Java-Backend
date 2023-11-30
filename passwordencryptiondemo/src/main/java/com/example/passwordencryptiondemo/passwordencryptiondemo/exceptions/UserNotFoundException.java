0a4be9aaacd908abf7148109fb45cc12a8e67cd2
akshayrock45 <77655354+akshayrock45@users.noreply.github.com>
2023-11-30 17:08:26 +0530

    Delete passwordencryptiondemo directory

--- passwordencryptiondemo/src/main/java/com/example/passwordencryptiondemo/passwordencryptiondemo/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.passwordencryptiondemo.passwordencryptiondemo.exceptions;
-
-public class UserNotFoundException extends RuntimeException
-{
-    public UserNotFoundException(String message)
-    {
-        super(message);
-    }
-}
--- passwordencryptiondemo/src/main/java/com/example/passwordencryptiondemo/passwordencryptiondemo/repositories/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.passwordencryptiondemo.passwordencryptiondemo.repositories;
-
-import com.example.passwordencryptiondemo.passwordencryptiondemo.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository <Users, String> {
-}
