4329daaec284889a9455e1e4fdd8130121b44569
Subham Saha <subhams56.ss@gmail.com>
2023-12-17 19:27:01 +0530

    API Integration V1.2

---
+++ Client/src/App.jsx
@@ -0,0 +1,40 @@
+
+import { Routes ,Route } from 'react-router-dom'
+import './index.css'
+import HomePage from './Pages/HomePage'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Employees from './Pages/Employees'
+import Inwards from './Pages/Inwards'
+import Outwards from './Pages/Outwards'
+import Deliveries from './Pages/Deliveries'
+import Godowns from './Pages/Godowns'
+import AdminAccount from './Pages/AdminAccount'
+import UserAccount from './Pages/UserAccount'
+
+function App() {
+ 
+
+  Routes
+    return (
+      <div>
+        <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/adminAccount" element={<AdminAccount/>}/>
+        <Route path="/userAccount" element={<UserAccount/>}/>
+        <Route path="/employees" element={<Employees/>}/>
+        <Route path="/godowns" element={<Godowns/>}/>
+        <Route path="/inwards" element={<Inwards/>}/>
+        <Route path="/outwards" element={<Outwards/>}/>
+        <Route path="/deliveries" element={<Deliveries/>}/>
+        
+        </Routes>
+      </div>
+     
+    )
+  }
+
+
+export default App
